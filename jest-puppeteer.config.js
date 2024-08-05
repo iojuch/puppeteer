39bedf3b9000fffafb62eec49d3eb246e3ce0691
Александр Забелинский <petrovich8384@mail.ru>
2024-08-06 00:29:52 +0300

    Initial commit

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	launch: {
+		//slowMo: 1000,
+        headless: false,
+        defaultViewport: null,
+        args: ['--start-maximized'] //— используем максимальный размер окна браузера
+    },
+};
